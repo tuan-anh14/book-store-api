3630542cc25d5ea7891c494801b65d5aa5c65dca
Thieu Tuan Anh <thieutuananh14092k4@gmail.com>
2025-04-24 08:35:20 +0700

    Setting env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) { }
 
   @Get()
   @Render('home')
   getHello() {
+    console.log('check port: ', this.configService.get<string>('PORT'));
+    //port from .env
     // return "this.appService.getHello()";
   }
 }
