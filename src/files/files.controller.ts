650e2309729f01e19f07ff6c49f0360c557ad7fb
Thieu Tuan Anh <thieutuananh14092k4@gmail.com>
2025-05-15 19:31:04 +0700

    Upload file

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller, Get, Post, Body, Patch, Param,
+  Delete, UseInterceptors, UploadedFile, ParseFilePipeBuilder, HttpStatus, UseFilters
+} from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Public, ResponseMessage } from 'src/decorator/customize';
+import { HttpExceptionFilter } from 'src/core/http-exception.filter';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService) { }
 
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto);
+  @Public()
+  @Post('upload')
+  @ResponseMessage("Upload Single File")
+  @UseInterceptors(FileInterceptor('fileUpload'))
+  @UseFilters(new HttpExceptionFilter())
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      fileName: file.filename
+    }
   }
 
   @Get()
